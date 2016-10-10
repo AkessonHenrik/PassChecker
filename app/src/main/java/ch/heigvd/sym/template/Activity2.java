52b3f3d15e25a3314cba28f08f42bab22eb8d8c4
AkessonHenrik <henrik.akesson@heig-vd.ch>
2016-10-10 14:08:51 +0200

    Picture works, IMEI needs mobile testing

--- app/src/main/java/ch/heigvd/sym/template/Activity2.java
+++ app/src/main/java/ch/heigvd/sym/template/Activity2.java
@@ -1,11 +1,81 @@
 package ch.heigvd.sym.template;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.ImageView;
 
-/**
- * Created by Henrik on 22.09.2016.
- */
+import java.io.File;
+
 
 public class Activity2 extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                }
+        });
+        showPicture();
+        showIMEI();
+    }
+
+    private void showPicture() {
+        File f = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        System.out.println("Files:");
+        ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+        for(File file : f.listFiles()) {
+            System.out.println("File: " + file.getName());
+
+            File imgFile = new  File(file.getAbsolutePath());
+
+            if(imgFile.exists()) {
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                imageView.setImageBitmap(myBitmap);
+            }
+
+        }
+    }
+
+    protected void showIMEI() {
+        String identifier = null;
+        TelephonyManager tm = (TelephonyManager)Activity2.this.getSystemService(Context.TELEPHONY_SERVICE);
+        if (tm != null)
+            identifier = tm.getDeviceId();
+        if (identifier == null || identifier .length() == 0)
+            identifier = Settings.Secure.getString(Activity2.this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        System.out.println("ID = " + identifier);
+        AlertDialog.Builder alertbd = new AlertDialog.Builder(this);
+        alertbd.setIcon(android.R.drawable.ic_dialog_alert);
+        alertbd.setTitle("IMEI");
+        alertbd.setMessage(identifier);
+        alertbd.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // we do nothing...
+                // dialog close automatically
+            }
+        });
+        alertbd.show();
+    }
 }
--- app/src/main/java/ch/heigvd/sym/template/MainActivity.java
+++ app/src/main/java/ch/heigvd/sym/template/MainActivity.java
@@ -1,32 +1,8 @@
-/**
- * File     : MainActivity.java
- * Project  : TemplateActivity
- * Author   : Markus Jaton 2 juillet 2014
- * 			  Fabien Dutoit 20 septembre 2016
- *            IICT / HEIG-VD
- *                                       
- * mailto:fabien.dutoit@heig-vd.ch
- * 
- * This piece of code reads a [email_account / password ] combination.
- * It is used as a template project for the SYM module element given at HEIG-VD
- * Target audience : students IL, TS, IE [generally semester 1, third bachelor year]
- *   
- * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
- * THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- */
 package ch.heigvd.sym.template;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -65,30 +41,17 @@ public class MainActivity extends AppCompatActivity {
 
 			@Override
 			public void onClick(View v) {
-				/*
-				 * There you have to check out if the email/password
-				 * combination given is valid or not
-				 */
+
 				String mail = email.getText().toString();
 				String pass = password.getText().toString(); //TODO read password from EditText
 				if (isValid(mail, pass)) {
-					/* Ok, valid combination, do something or launch another activity...
-					 * The current activity could be finished, but it is not mandatory.
-					 * To launch activity MyActivity.class, try something like :
-					 * 
-					 * 			Intent intent = new Intent(this, ch.heigvd.sym.MyActivity.class);
-					 * 			intent.putExtra("emailEntered", mail);
-					 *			intent.putExtra("passwordGiven", passwd);
-					 *			this.startActivity(intent); 
-					 *
-					 * Alternately, you could also startActivityForResult if you are awaiting a result.
-					 * In the latter case, you have to indicate an int parameter to identify MyActivity
-					 * 
-					 * If you haven't anything more to do, you may finish()...
-					 * But just display a small message before quitting...
-					 */
-					Toast.makeText(MainActivity.this, getResources().getString(R.string.good), Toast.LENGTH_LONG).show();
-//					finish();
+					Toast.makeText(MainActivity.this, getResources().getString(R.string.goodLogin), Toast.LENGTH_LONG).show();
+					Intent intent = new Intent(MainActivity.this, Activity2.class);
+					intent.putExtra("Email", mail);
+					intent.putExtra("Password", pass);
+					startActivity(intent);
+
+					finish();
 				}
 			}
 
@@ -119,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
 			}
 			if(exists) {
 				//Handle success
+
 				return true;
 			} else {
 				showErrorDialog(mail, passwd);
@@ -128,14 +92,11 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		// Return true if combination valid, false otherwise
-//		return (mail.equals(validEmail) && passwd.equals(validPassword));
 		return false;
 	}
 
 	protected void showErrorDialog(String mail, String passwd) {
-		/*
-		 * Pop-up dialog to show error
-		 */
+
 		AlertDialog.Builder alertbd = new AlertDialog.Builder(this);
         alertbd.setIcon(android.R.drawable.ic_dialog_alert);
 		alertbd.setTitle(R.string.wronglogin);
